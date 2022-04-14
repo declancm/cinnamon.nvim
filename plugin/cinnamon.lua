f8afc6230e2349880d08b37d5d6ca0e97ab4a7f1
declancm <declanmullen3.14@gmail.com>
2022-04-14 23:02:13 +1000

    Use protected call for setup.

--- plugin/cinnamon.lua
+++ plugin/cinnamon.lua
@@ -1,3 +1,11 @@
-if not vim.g.__cinnamon_setup_loaded then
-  require('cinnamon').setup()
+if vim.g.__cinnamon_setup_loaded then
+  return
 end
+
+local cinnamon_status, cinnamon = pcall(require, 'cinnamon')
+if not cinnamon_status then
+  print('Error: cinnamon setup failed')
+  return
+end
+
+cinnamon.setup()
