85db921212f379bbb43532bc1792f891c3533a0a
Declan Mullen <declanmullen3.14@gmail.com>
2024-06-20 21:07:09 -0700

    Update keymaps to use new function

--- lua/cinnamon/init.lua
+++ lua/cinnamon/init.lua
@@ -3,14 +3,10 @@ local M = {}
 local map = function(mode, lhs, rhs)
     if type(mode) == "table" then
         for _, value in ipairs(mode) do
-            if vim.fn.maparg(lhs, value) == "" or config.override_keymaps then
-                vim.api.nvim_set_keymap(value, lhs, rhs, { noremap = true })
-            end
+            vim.keymap.set(value, lhs, rhs)
         end
     else
-        if vim.fn.maparg(lhs, mode) == "" or config.override_keymaps then
-            vim.api.nvim_set_keymap(mode, lhs, rhs, { noremap = true })
-        end
+        vim.keymap.set(mode, lhs, rhs)
     end
 end
 
@@ -49,82 +45,90 @@ M.setup = function(user_config)
     ]])
     end
 
+    local s = require("cinnamon.scroll").scroll
+
+    -- stylua: ignore start
     if config.default_keymaps then
         -- Half-window movements:
-        map({ "n", "x" }, "<C-u>", "<Cmd>lua Scroll('<C-u>', 1, 1)<CR>")
-        map({ "n", "x" }, "<C-d>", "<Cmd>lua Scroll('<C-d>', 1, 1)<CR>")
+        map({ "n", "x" }, "<C-u>", function() s("<C-u>") end)
+        map({ "n", "x" }, "<C-d>", function() s("<C-d>") end)
 
         -- Page movements:
-        map({ "n", "x" }, "<C-b>", "<Cmd>lua Scroll('<C-b>', 0, 1)<CR>")
-        map({ "n", "x" }, "<C-f>", "<Cmd>lua Scroll('<C-f>', 0, 1)<CR>")
-        map({ "n", "x" }, "<PageUp>", "<Cmd>lua Scroll('<C-b>', 0, 1)<CR>")
-        map({ "n", "x" }, "<PageDown>", "<Cmd>lua Scroll('<C-f>', 0, 1)<CR>")
+        map({ "n", "x" }, "<C-b>", function() s("<C-b>") end)
+        map({ "n", "x" }, "<C-f>", function() s("<C-f>") end)
+        map({ "n", "x" }, "<PageUp>", function() s("<PageUp>") end)
+        map({ "n", "x" }, "<PageDown>", function() s("<PageDown>") end)
     end
 
     if config.extra_keymaps then
         -- Start/end of file and line number movements:
-        map({ "n", "x" }, "gg", "<Cmd>lua Scroll('gg', 0, 1)<CR>")
-        map({ "n", "x" }, "G", "<Cmd>lua Scroll('G', 0, 1)<CR>")
+        map({ "n", "x" }, "gg", function() s("gg") end)
+        map({ "n", "x" }, "G", function() s("G") end)
 
         -- Start/end of line:
-        map({ "n", "x" }, "0", "<Cmd>lua Scroll('0')<CR>")
-        map({ "n", "x" }, "^", "<Cmd>lua Scroll('^')<CR>")
-        map({ "n", "x" }, "$", "<Cmd>lua Scroll('$', 0, 1)<CR>")
+        map({ "n", "x" }, "0", function() s("0") end)
+        map({ "n", "x" }, "^", function() s("^") end)
+        map({ "n", "x" }, "$", function() s("$") end)
 
         -- Paragraph movements:
-        map({ "n", "x" }, "{", "<Cmd>lua Scroll('{')<CR>")
-        map({ "n", "x" }, "}", "<Cmd>lua Scroll('}')<CR>")
+        map({ "n", "x" }, "{", function()
+            s("{")
+        end)
+        map({ "n", "x" }, "}", function() s("}") end)
 
         -- Previous/next search result:
-        map("n", "n", "<Cmd>lua Scroll('n', 1)<CR>")
-        map("n", "N", "<Cmd>lua Scroll('N', 1)<CR>")
-        map("n", "*", "<Cmd>lua Scroll('*', 1)<CR>")
-        map("n", "#", "<Cmd>lua Scroll('#', 1)<CR>")
-        map("n", "g*", "<Cmd>lua Scroll('g*', 1)<CR>")
-        map("n", "g#", "<Cmd>lua Scroll('g#', 1)<CR>")
+        map("n", "n", function() s("n") end)
+        map("n", "N", function() s("N") end)
+        map("n", "*", function() s("*") end)
+        map("n", "#", function() s("#") end)
+        map("n", "g*", function() s("g*") end)
+        map("n", "g#", function() s("g#") end)
 
         -- Previous/next cursor location:
-        map("n", "<C-o>", "<Cmd>lua Scroll('<C-o>', 1)<CR>")
-        map("n", "<C-i>", "<Cmd>lua Scroll('1<C-i>', 1)<CR>")
+        map("n", "<C-o>", function() s("<C-o>") end)
+        map("n", "<C-i>", function() s("<C-i>") end)
 
         -- Screen scrolling:
-        map("n", "zz", "<Cmd>lua Scroll('zz', 0, 1)<CR>")
-        map("n", "zt", "<Cmd>lua Scroll('zt', 0, 1)<CR>")
-        map("n", "zb", "<Cmd>lua Scroll('zb', 0, 1)<CR>")
-        map("n", "z.", "<Cmd>lua Scroll('z.', 0, 1)<CR>")
-        map("n", "z<CR>", "<Cmd>lua Scroll('zt^', 0, 1)<CR>")
-        map("n", "z-", "<Cmd>lua Scroll('z-', 0, 1)<CR>")
-        map("n", "z^", "<Cmd>lua Scroll('z^', 0, 1)<CR>")
-        map("n", "z+", "<Cmd>lua Scroll('z+', 0, 1)<CR>")
-        map("n", "<C-y>", "<Cmd>lua Scroll('<C-y>', 0, 1)<CR>")
-        map("n", "<C-e>", "<Cmd>lua Scroll('<C-e>', 0, 1)<CR>")
+        map("n", "zz", function() s("zz") end)
+        map("n", "zt", function() s("zt") end)
+        map("n", "zb", function() s("zb") end)
+        map("n", "z.", function() s("z.") end)
+        map("n", "z<CR>", function() s("z<CR>") end)
+        map("n", "z-", function() s("z-") end)
+        map("n", "z^", function() s("z^") end)
+        map("n", "z+", function() s("z+") end)
+        map("n", "<C-y>", function() s("<C-y>") end)
+        map("n", "<C-e>", function() s("<C-e>") end)
 
         -- Horizontal screen scrolling:
-        map("n", "zh", "<Cmd>lua Scroll('zh', 0, 1)<CR>")
-        map("n", "zl", "<Cmd>lua Scroll('zl', 0, 1)<CR>")
-        map("n", "zH", "<Cmd>lua Scroll('zH')<CR>")
-        map("n", "zL", "<Cmd>lua Scroll('zL')<CR>")
-        map("n", "zs", "<Cmd>lua Scroll('zs')<CR>")
-        map("n", "ze", "<Cmd>lua Scroll('ze')<CR>")
+        map("n", "zh", function() s("zh") end)
+        map("n", "zl", function() s("zl") end)
+        map("n", "zH", function() s("zH") end)
+        map("n", "zL", function() s("zL") end)
+        map("n", "zs", function() s("zs") end)
+        map("n", "ze", function() s("ze") end)
     end
 
     if config.extended_keymaps then
         -- Up/down movements:
-        map({ "n", "x" }, "k", "<Cmd>lua Scroll('k', 0, 1)<CR>")
-        map({ "n", "x" }, "j", "<Cmd>lua Scroll('j', 0, 1)<CR>")
-        map({ "n", "x" }, "<Up>", "<Cmd>lua Scroll('<Up>', 0, 1)<CR>")
-        map({ "n", "x" }, "<Down>", "<Cmd>lua Scroll('<Down>', 0, 1)<CR>")
-        map({ "n", "x" }, "gk", "<Cmd>lua Scroll('gk', 0, 1)<CR>")
-        map({ "n", "x" }, "gj", "<Cmd>lua Scroll('gj', 0, 1)<CR>")
-        map({ "n", "x" }, "g<Up>", "<Cmd>lua Scroll('g<Up>', 0, 1)<CR>")
-        map({ "n", "x" }, "g<Down>", "<Cmd>lua Scroll('g<Down>', 0, 1)<CR>")
+        map({ "n", "x" }, "k", function() s("k") end)
+        map({ "n", "x" }, "j", function() s("j") end)
+        map({ "n", "x" }, "<Up>", function() s("<Up>") end)
+        map({ "n", "x" }, "<Down>", function() s("<Down>") end)
+        map({ "n", "x" }, "gk", function() s("gk") end)
+        map({ "n", "x" }, "gj", function()
+            s("gj")
+        end)
+        map({ "n", "x" }, "g<Up>", function() s("g<Up>") end)
+        map({ "n", "x" }, "g<Down>", function() s("g<Down>") end)
 
         -- Left/right movements:
-        map({ "n", "x" }, "h", "<Cmd>lua Scroll('h', 0, 1)<CR>")
-        map({ "n", "x" }, "l", "<Cmd>lua Scroll('l', 0, 1)<CR>")
-        map({ "n", "x" }, "<Left>", "<Cmd>lua Scroll('<Left>', 0, 1)<CR>")
-        map({ "n", "x" }, "<Right>", "<Cmd>lua Scroll('<Right>', 0, 1)<CR>")
+        map({ "n", "x" }, "h", function() s("h") end)
+        map({ "n", "x" }, "l", function() s("l") end)
+        map({ "n", "x" }, "<Left>", function() s("<Left>") end)
+        map({ "n", "x" }, "<Right>", function() s("<Right>") end)
     end
+    --stylua: ignore end
 end
 
 M.scroll = require("cinnamon.scroll").scroll
