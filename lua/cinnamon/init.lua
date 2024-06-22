a1461f1db3775e54dc856303ba8a8dfb5cd54acf
Declan Mullen <declanmullen3.14@gmail.com>
2024-06-22 11:27:34 -0700

    Use vim.keymap.set

--- lua/cinnamon/init.lua
+++ lua/cinnamon/init.lua
@@ -1,15 +1,5 @@
 local M = {}
 
-local map = function(mode, lhs, rhs)
-    if type(mode) == "table" then
-        for _, value in ipairs(mode) do
-            vim.keymap.set(value, lhs, rhs)
-        end
-    else
-        vim.keymap.set(mode, lhs, rhs)
-    end
-end
-
 M.setup = function(user_config)
     local config = require("cinnamon.config")
 
@@ -35,79 +25,79 @@ M.setup = function(user_config)
     -- stylua: ignore start
     if config.default_keymaps then
         -- Half-window movements:
-        map({ "n", "x" }, "<C-u>", function() s("<C-u>") end)
-        map({ "n", "x" }, "<C-d>", function() s("<C-d>") end)
+        vim.keymap.set({ "n", "x" }, "<C-u>", function() s("<C-u>") end)
+        vim.keymap.set({ "n", "x" }, "<C-d>", function() s("<C-d>") end)
 
         -- Page movements:
-        map({ "n", "x" }, "<C-b>", function() s("<C-b>") end)
-        map({ "n", "x" }, "<C-f>", function() s("<C-f>") end)
-        map({ "n", "x" }, "<PageUp>", function() s("<PageUp>") end)
-        map({ "n", "x" }, "<PageDown>", function() s("<PageDown>") end)
+        vim.keymap.set({ "n", "x" }, "<C-b>", function() s("<C-b>") end)
+        vim.keymap.set({ "n", "x" }, "<C-f>", function() s("<C-f>") end)
+        vim.keymap.set({ "n", "x" }, "<PageUp>", function() s("<PageUp>") end)
+        vim.keymap.set({ "n", "x" }, "<PageDown>", function() s("<PageDown>") end)
     end
 
     if config.extra_keymaps then
         -- Start/end of file and line number movements:
-        map({ "n", "x" }, "gg", function() s("gg") end)
-        map({ "n", "x" }, "G", function() s("G") end)
+        vim.keymap.set({ "n", "x" }, "gg", function() s("gg") end)
+        vim.keymap.set({ "n", "x" }, "G", function() s("G") end)
 
         -- Start/end of line:
-        map({ "n", "x" }, "0", function() s("0") end)
-        map({ "n", "x" }, "^", function() s("^") end)
-        map({ "n", "x" }, "$", function() s("$") end)
+        vim.keymap.set({ "n", "x" }, "0", function() s("0") end)
+        vim.keymap.set({ "n", "x" }, "^", function() s("^") end)
+        vim.keymap.set({ "n", "x" }, "$", function() s("$") end)
 
         -- Paragraph movements:
-        map({ "n", "x" }, "{", function() s("{") end)
-        map({ "n", "x" }, "}", function() s("}") end)
+        vim.keymap.set({ "n", "x" }, "{", function() s("{") end)
+        vim.keymap.set({ "n", "x" }, "}", function() s("}") end)
 
         -- Previous/next search result:
-        map("n", "n", function() s("n") end)
-        map("n", "N", function() s("N") end)
-        map("n", "*", function() s("*") end)
-        map("n", "#", function() s("#") end)
-        map("n", "g*", function() s("g*") end)
-        map("n", "g#", function() s("g#") end)
+        vim.keymap.set("n", "n", function() s("n") end)
+        vim.keymap.set("n", "N", function() s("N") end)
+        vim.keymap.set("n", "*", function() s("*") end)
+        vim.keymap.set("n", "#", function() s("#") end)
+        vim.keymap.set("n", "g*", function() s("g*") end)
+        vim.keymap.set("n", "g#", function() s("g#") end)
 
         -- Previous/next cursor location:
-        map("n", "<C-o>", function() s("<C-o>") end)
-        map("n", "<C-i>", function() s("<C-i>") end)
+        vim.keymap.set("n", "<C-o>", function() s("<C-o>") end)
+        vim.keymap.set("n", "<C-i>", function() s("<C-i>") end)
 
         -- Screen scrolling:
-        map("n", "zz", function() s("zz") end)
-        map("n", "zt", function() s("zt") end)
-        map("n", "zb", function() s("zb") end)
-        map("n", "z.", function() s("z.") end)
-        map("n", "z<CR>", function() s("z<CR>") end)
-        map("n", "z-", function() s("z-") end)
-        map("n", "z^", function() s("z^") end)
-        map("n", "z+", function() s("z+") end)
-        map("n", "<C-y>", function() s("<C-y>") end)
-        map("n", "<C-e>", function() s("<C-e>") end)
+        vim.keymap.set("n", "zz", function() s("zz") end)
+        vim.keymap.set("n", "zt", function() s("zt") end)
+        vim.keymap.set("n", "zb", function() s("zb") end)
+        vim.keymap.set("n", "z.", function() s("z.") end)
+        vim.keymap.set("n", "z<CR>", function() s("z<CR>") end)
+        vim.keymap.set("n", "z-", function() s("z-") end)
+        vim.keymap.set("n", "z^", function() s("z^") end)
+        vim.keymap.set("n", "z+", function() s("z+") end)
+        vim.keymap.set("n", "<C-y>", function() s("<C-y>") end)
+        vim.keymap.set("n", "<C-e>", function() s("<C-e>") end)
 
         -- Horizontal screen scrolling:
-        map("n", "zh", function() s("zh") end)
-        map("n", "zl", function() s("zl") end)
-        map("n", "zH", function() s("zH") end)
-        map("n", "zL", function() s("zL") end)
-        map("n", "zs", function() s("zs") end)
-        map("n", "ze", function() s("ze") end)
+        vim.keymap.set("n", "zh", function() s("zh") end)
+        vim.keymap.set("n", "zl", function() s("zl") end)
+        vim.keymap.set("n", "zH", function() s("zH") end)
+        vim.keymap.set("n", "zL", function() s("zL") end)
+        vim.keymap.set("n", "zs", function() s("zs") end)
+        vim.keymap.set("n", "ze", function() s("ze") end)
     end
 
     if config.extended_keymaps then
         -- Up/down movements:
-        map({ "n", "x" }, "k", function() s("k") end)
-        map({ "n", "x" }, "j", function() s("j") end)
-        map({ "n", "x" }, "<Up>", function() s("<Up>") end)
-        map({ "n", "x" }, "<Down>", function() s("<Down>") end)
-        map({ "n", "x" }, "gk", function() s("gk") end)
-        map({ "n", "x" }, "gj", function() s("gj") end)
-        map({ "n", "x" }, "g<Up>", function() s("g<Up>") end)
-        map({ "n", "x" }, "g<Down>", function() s("g<Down>") end)
+        vim.keymap.set({ "n", "x" }, "k", function() s("k") end)
+        vim.keymap.set({ "n", "x" }, "j", function() s("j") end)
+        vim.keymap.set({ "n", "x" }, "<Up>", function() s("<Up>") end)
+        vim.keymap.set({ "n", "x" }, "<Down>", function() s("<Down>") end)
+        vim.keymap.set({ "n", "x" }, "gk", function() s("gk") end)
+        vim.keymap.set({ "n", "x" }, "gj", function() s("gj") end)
+        vim.keymap.set({ "n", "x" }, "g<Up>", function() s("g<Up>") end)
+        vim.keymap.set({ "n", "x" }, "g<Down>", function() s("g<Down>") end)
 
         -- Left/right movements:
-        map({ "n", "x" }, "h", function() s("h") end)
-        map({ "n", "x" }, "l", function() s("l") end)
-        map({ "n", "x" }, "<Left>", function() s("<Left>") end)
-        map({ "n", "x" }, "<Right>", function() s("<Right>") end)
+        vim.keymap.set({ "n", "x" }, "h", function() s("h") end)
+        vim.keymap.set({ "n", "x" }, "l", function() s("l") end)
+        vim.keymap.set({ "n", "x" }, "<Left>", function() s("<Left>") end)
+        vim.keymap.set({ "n", "x" }, "<Right>", function() s("<Right>") end)
     end
     --stylua: ignore end
 end
