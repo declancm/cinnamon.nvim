7f8af92a59da9e07bdac289a423cbd15e910eea3
Declan Mullen <declanmullen3.14@gmail.com>
2024-06-27 21:39:01 -0700

    fix: improve the deprecated config conversion

--- lua/cinnamon/config.lua
+++ lua/cinnamon/config.lua
@@ -18,17 +18,17 @@ local defaults = {
 }
 
 local deprecated = {
-    ["default_keymaps"] = "The 'default_keymaps' option has been deprecated. Use 'keymaps.basic'.",
-    ["extra_keymaps"] = "The 'extra_keymaps' option has beeen deprecated. Use 'keymaps.basic' and/or 'keymaps.extra'.",
-    ["extended_keymaps"] = "The 'extended_keymaps' option has been deprecated. Use 'keymaps.extra'.",
-    ["override_keymaps"] = "The 'override_keymaps' option has been removed.",
-    ["always_scroll"] = "The 'always_scroll' option has been deprecated. Use 'options.mode == \"screen\"'.",
-    ["centered"] = "The 'centered' option has been deprecated. Append 'zz' to your command to replicate.",
-    ["default_delay"] = "The 'default_delay' option has been deprecated. Use 'options.delay'.",
-    ["hide_cursor"] = "The 'hide_cursor' option has been removed. The cursor is hidden when using 'options.mode = \"screen\".",
-    ["horizontal_scroll"] = "The 'horizontal_scroll' option has been removed. Horizontal scrolling is always enabled.",
-    ["max_length"] = "The 'max_length' option has been deprecated. Use 'options.max_delta'.",
-    ["scroll_limit"] = "The 'scroll_limit' option has been deprecated. Use 'options.max_delta'.",
+    ["default_keymaps"] = "The 'default_keymaps' option has been deprecated. Please use 'keymaps.basic' instead",
+    ["extra_keymaps"] = "The 'extra_keymaps' option has beeen deprecated. Please use 'keymaps.extra' instead",
+    ["extended_keymaps"] = "The 'extended_keymaps' option has been deprecated. Please use 'keymaps.extra' instead",
+    ["override_keymaps"] = "The 'override_keymaps' option has been removed",
+    ["always_scroll"] = "The 'always_scroll' option has been deprecated. Please use 'options.mode' instead",
+    ["centered"] = "The 'centered' option has been deprecated. Append 'zz' to your command to replicate",
+    ["default_delay"] = "The 'default_delay' option has been deprecated. Please use 'options.delay' instead",
+    ["hide_cursor"] = "The 'hide_cursor' option has been removed. The cursor is hidden when using 'options.mode = \"window\"",
+    ["horizontal_scroll"] = "The 'horizontal_scroll' option has been removed. Horizontal scrolling is always enabled",
+    ["max_length"] = "The 'max_length' option has been deprecated. Please use 'options.max_delta' instead",
+    ["scroll_limit"] = "The 'scroll_limit' option has been deprecated. Please use 'options.max_delta' instead",
 }
 
 local config = {}
@@ -49,34 +49,21 @@ function M.setup(user_config)
     end
 
     -- Convert deprecated options to new options.
-    if user_config.default_keymaps ~= nil then
-        user_config.keymaps = {
-            basic = user_config.default_keymaps,
-        }
-    end
-    if user_config.extra_keymaps ~= nil then
-        user_config.keymaps = {
-            basic = user_config.extra_keymaps,
-            extra = user_config.extra_keymaps,
-        }
-    end
-    if user_config.extended_keymaps == true then
-        user_config.keymaps = {
-            extra = true,
-        }
-    end
-    if user_config.default_delay ~= nil then
-        user_config.options = {
-            delay = user_config.default_delay,
-        }
-    end
+    user_config.keymaps = user_config.keymaps or {}
+    user_config.keymaps.basic = user_config.keymaps.basic or user_config.default_keymaps or user_config.extra_keymaps
+    user_config.keymaps.extra = user_config.keymaps.extra or user_config.extra_keymaps or user_config.extended_keymaps
+
+    user_config.options = user_config.options or {}
+    user_config.options.delay = user_config.options.delay or user_config.default_delay
+    user_config.options.max_delta = user_config.options.max_delta or {}
     if user_config.scroll_limit ~= nil then
-        user_config.options = {
-            max_delta = {
-                line = (user_config.scroll_limit <= 0) and 9999 or user_config.scroll_limit,
-            },
-        }
+        if user_config.scroll_limit < 0 then
+            user_config.options.max_delta.line = 9999
+        else
+            user_config.options.max_delta.line = user_config.scroll_limit
+        end
     end
+    vim.print(user_config)
 
     -- Merge user options with defaults.
     config = vim.tbl_deep_extend("force", {}, defaults, user_config)
